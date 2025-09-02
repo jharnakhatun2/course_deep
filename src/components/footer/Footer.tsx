ea324b7d6a1348d076ef1033d99c23931c3510cc
jharnakhatun2 <jharnakhatun203@gmail.com>
2025-09-02 10:45:38 +0600

    Added responsive mobile menu

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,40 +1,47 @@
-import { Link } from "react-router"
-import BottomFooter from "./BottomFooter"
-import Button from "../../ult/button/Button"
-import ContactUs from "./ContactUs"
+import { Link } from "react-router";
+import BottomFooter from "./BottomFooter";
+import Button from "../../ult/button/Button";
+import ContactUs from "./ContactUs";
 
 const quickLinks = [
-    { label: "All Courses", href: "/" },
-    { label: "Summer Sessions", href: "/" },
-    { label: "Recent Exams", href: "/" },
-    { label: "Professional Courses", href: "/" },
-    { label: "Portfolio", href: "/" },
-  ]
+  { label: "All Courses", href: "/" },
+  { label: "Summer Sessions", href: "/" },
+  { label: "Recent Exams", href: "/" },
+  { label: "Professional Courses", href: "/" },
+  { label: "Portfolio", href: "/" },
+];
 
 const Footer = () => {
-  const headerClass = "font-bold tracking-wide pb-2 border-b border-zinc-600 uppercase text-sm";
+  const headerClass =
+    "font-bold tracking-wide pb-2 border-b border-zinc-600 uppercase text-sm";
 
   return (
-    <div  className="bg-zinc-800 text-zinc-500 text-md">
-    <div className="px-4 pt-10 mx-auto lg:max-w-7xl">
-      <div className="space-y-6 grid sm:grid-cols-4 gap-5 lg:gap-10">
-        {/* First col */}
-        <div className="">
-          <Link to="/">
-            <span className="font-garamond text-xl text-yellow-500 font-bold tracking-wide uppercase">
-              Course Deep
-            </span>
-          </Link>
-          <div className="mt-4 lg:max-w-sm">
-            With 20+ years of experience, we deliver expert programming courses for businesses and individuals. Our instructors bring real-world expertise to help you master coding, project structuring, and building scalable applications.
+    <div className="bg-zinc-800 text-zinc-500 text-md">
+      <div className="px-4 pt-10 mx-auto lg:max-w-7xl">
+        <div className="space-y-6 grid sm:grid-cols-4 gap-5 lg:gap-10">
+          {/* First col */}
+          <div className="">
+            <Link to="/">
+              <span className="font-garamond text-xl text-yellow-500 font-bold tracking-wide uppercase">
+                Course Deep
+              </span>
+            </Link>
+            <div className="mt-4 lg:max-w-sm">
+              With 20+ years of experience, we deliver expert programming
+              courses for businesses and individuals. Our instructors bring
+              real-world expertise to help you master coding, project
+              structuring, and building scalable applications.
+            </div>
+            <Button
+              url="/courses"
+              className="border border-zinc-500 hover:text-white hover:bg-stone-400 hover:border-stone-400 mt-4 text-[12px]"
+            >
+              Start Learning Now
+            </Button>
           </div>
-          <Button url="/courses" className="border border-zinc-500 hover:text-white hover:bg-stone-400 hover:border-stone-400 mt-4 text-[12px]">Start Learning Now</Button>
-        </div>
-        {/* Other cols */}
+          {/* Other cols */}
           <div className="hidden sm:block">
-            <p className={headerClass}>
-              Popular Courses
-            </p>
+            <p className={headerClass}>Popular Courses</p>
             <ul className="mt-2 space-y-2">
               <li>
                 <a
@@ -71,34 +78,29 @@ const Footer = () => {
             </ul>
           </div>
           <div>
-            <p className={headerClass}>
-              Quick Links
-            </p>
+            <p className={headerClass}>Quick Links</p>
             <ul className="mt-2 space-y-2 list-disc pl-5">
               {quickLinks.map((link, index) => (
                 <li key={index}>
-                <Link
-                  to={link.href}
-                  className=" transition-colors duration-300 hover:text-zinc-100"
-                >
-                  {link.label}
-                </Link>
-              </li>
-                ))}
-              
+                  <Link
+                    to={link.href}
+                    className=" transition-colors duration-300 hover:text-zinc-100"
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
             </ul>
           </div>
           <div className="mb-5">
-            <p className={headerClass}>
-              Contact Us
-            </p>
+            <p className={headerClass}>Contact Us</p>
             <ContactUs />
           </div>
+        </div>
+        <BottomFooter />
       </div>
-      <BottomFooter />
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/navigation/Menu.tsx
+++ src/components/navigation/Menu.tsx
@@ -1,9 +1,86 @@
+import { Link } from "react-router";
 
+const menuList = [
+  { name: "Home", path: "/" },
+  { name: "Courses", path: "/" },
+  { name: "Events", path: "/" },
+  { name: "Blog", path: "/" },
+  { name: "Contact", path: "/" },
+];
 
 const Menu = () => {
   return (
-    <div>Menu</div>
-  )
-}
+    <nav className="bg-yellow-400 py-3 sm:py-0">
+      <div className="lg:max-w-7xl mx-auto px-4 ">
+        <div className="flex justify-between items-center">
+          {/* <!-- mobile button goes here --> */}
+          <div className="md:hidden flex items-center">
+            <button className="mobile-menu-button cursor-pointer">
+              <svg
+                className="w-8 h-8 border p-1 rounded text-zinc-700 border-zinc-700"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </button>
+          </div>
 
-export default Menu
+          <div className="flex space-x-4">
+            {/* <!-- primary nav --> */}
+            <div className="hidden md:flex items-center space-x-10 text-white">
+              {menuList.map((item, index) => (
+                <Link
+                  key={index}
+                  to={item.path}
+                  className="hover:text-zinc-600 transition-smooth uppercase text-sm"
+                >
+                  {item.name}
+                </Link>
+              ))}
+            </div>
+          </div>
+
+          {/* <!-- Login & Signup --> */}
+          <div className="flex items-center space-x-1 text-white bg-gray-500/30 px-3 h-8 rounded">
+            <Link
+              to="/login"
+              className="cursor-pointer hover:text-black transition-smooth"
+            >
+              Login
+            </Link>
+            <span>|</span>
+            <Link
+              to="/signup"
+              className="cursor-pointer hover:text-black transition-smooth"
+            >
+              Signup
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      {/* <!-- mobile menu --> */}
+      <div className="mobile-menu md:hidden space-y-3 pl-4 text-zinc-700 pt-5 pb-3">
+        {menuList.map((item, index) => (
+                <Link
+                  key={index}
+                  to={item.path}
+                  className="flex hover:font-bold transition-smooth uppercase text-[12px] "
+                >
+                  {item.name}
+                </Link>
+              ))}
+      </div>
+    </nav>
+  );
+};
+
+export default Menu;
