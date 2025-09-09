16e4169c17e67222f918ab3763824a1dc5351629
jharnakhatun2 <jharnakhatun203@gmail.com>
2025-09-09 20:48:00 +0600

    Added Login form and registration form with RTK query successful

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
-import { useAuth } from "./UserContext";
+import { useRegisterMutation, useLoginMutation } from "../../features/auth/authApi";
+import { setCredentials } from "../../features/auth/authSlice";
 import { showErrorToast, showSuccessToast } from "../../ult/toast/toast";
-import Button from "../../ult/button/Button";
-import { FcGoogle } from "react-icons/fc";
+import { useAppDispatch } from "../../app/hooks";
 
 interface FormData {
   name?: string;
@@ -18,10 +18,14 @@ const Login = () => {
     email: "",
     password: "",
   });
-  const { createUser, userLogIn, googleSignIn } = useAuth();
+
+  const [registerUser, { isLoading: isRegistering }] = useRegisterMutation();
+  const [loginUser, { isLoading: isLoggingIn }] = useLoginMutation();
+
+  const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
-  // Sync input data structure when newAccount toggles
+  // Sync input fields when toggling between login/register
   useEffect(() => {
     setFormData(
       newAccount
@@ -35,50 +39,52 @@ const Login = () => {
     setNewAccount((prev) => !prev);
   };
 
-  // handle input change dynamically
+  // handle input change
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
   // handle form submit
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("Form Data:", formData);
-    const { email, password } = formData;
 
-    if (newAccount) {
-      //create user
-      createUser(email, password)
-        .then((result) => {
-          showSuccessToast("Account created successfully!");
-          console.log("Create User Successfully", result);
-          setFormData({ name: "", email: "", password: "" });
-          navigate("/");
-        })
-        .catch((error) => console.error(error));
-    } else {
-      //login user
-      userLogIn(email, password)
-        .then((result) => {
-          showSuccessToast("Logged in successfully!");
-          console.log("User Login Successfully", result);
-          setFormData({ email: "", password: "" });
-          navigate("/");
-        })
-        .catch((error) => showErrorToast(error.message));
+    const { name, email, password } = formData;
+
+    // Basic validation
+    if (!email || !password || (newAccount && !name)) {
+      showErrorToast("Please fill in all required fields!");
+      return;
+    }
+
+    try {
+      if (newAccount) {
+        // ✅ Register
+        const res = await registerUser({ name, email, password }).unwrap();
+        showSuccessToast(res.message || "Account created successfully!");
+        setFormData({ name: "", email: "", password: "" });
+        setNewAccount(false); // switch to login after registration
+      } else {
+        // ✅ Login
+        const res = await loginUser({ email, password }).unwrap();
+
+        // Save user + token in Redux
+        dispatch(
+          setCredentials({
+            user: { email, name, role: "user" }, // server returns role in token
+            token: res.token,
+          })
+        );
+
+        showSuccessToast("Logged in successfully!");
+        setFormData({ email: "", password: "" });
+        navigate("/");
+      }
+    } catch (error: any) {
+      showErrorToast(error?.data?.message || "Something went wrong!");
     }
   };
 
-  const handleGoogleSignIn = () => {
-    googleSignIn()
-      .then((result) => {
-        showSuccessToast("Logged in successfully!");
-        console.log("User Login Successfully", result);
-        navigate("/");
-      })
-      .catch((error) => showErrorToast(error.message));
-  };
   return (
     <div className="relative py-3 sm:max-w-xl sm:mx-auto my-4 sm:my-20">
       <div className="absolute inset-0 bg-gradient-to-r from-teal-300 to-teal-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
@@ -92,28 +98,19 @@ const Login = () => {
             <form onSubmit={handleSubmit}>
               {/* Name field (only for register) */}
               {newAccount && (
-                <div className="form-control mt-4 ">
+                <div className="form-control mt-4">
                   <label className="label">
                     <span className="label-text">Name</span>
                   </label>
-                  <label className="input input-bordered flex items-center gap-2 w-full bg-white border border-zinc-200 focus:outline-yellow-700">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 16 16"
-                      fill="currentColor"
-                      className="w-4 h-4 opacity-70"
-                    >
-                      <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm4 1H4a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a2 2 0 0 0-2-2Z" />
-                    </svg>
-                    <input
-                      type="text"
-                      name="name"
-                      value={formData.name}
-                      onChange={handleChange}
-                      className="grow pl-2"
-                      placeholder="Enter Name"
-                    />
-                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    className="input input-bordered w-full bg-white border border-zinc-200"
+                    placeholder="Enter Name"
+                    required={newAccount}
+                  />
                 </div>
               )}
 
@@ -122,26 +119,15 @@ const Login = () => {
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
-                <label className="input input-bordered flex items-center gap-2 w-full bg-white border border-zinc-200 focus:border-black">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    className="w-4 h-4 opacity-70"
-                  >
-                    <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-                    <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-                  </svg>
-                  <input
-                    type="email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    className="grow pl-2"
-                    placeholder="email@example.com"
-                    required
-                  />
-                </label>
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  className="input input-bordered w-full bg-white border border-zinc-200"
+                  placeholder="email@example.com"
+                  required
+                />
               </div>
 
               {/* Password field */}
@@ -149,29 +135,15 @@ const Login = () => {
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
-                <label className="input input-bordered flex items-center gap-2 w-full bg-white border border-zinc-200">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    className="w-4 h-4 opacity-70"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                  <input
-                    type="password"
-                    name="password"
-                    value={formData.password}
-                    onChange={handleChange}
-                    className="grow pl-2"
-                    placeholder="Enter password"
-                    required
-                  />
-                </label>
+                <input
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  className="input input-bordered w-full bg-white border border-zinc-200"
+                  placeholder="Enter password"
+                  required
+                />
               </div>
 
               {/* Submit button */}
@@ -179,13 +151,19 @@ const Login = () => {
                 <button
                   type="submit"
                   className="btn bg-yellow-500 w-full uppercase rounded border-0"
+                  disabled={isRegistering || isLoggingIn}
                 >
-                  {newAccount ? "Register" : "Login"}
+                  {isRegistering || isLoggingIn
+                    ? "Processing..."
+                    : newAccount
+                    ? "Register"
+                    : "Login"}
                 </button>
               </div>
             </form>
 
-            <div className="text-center">
+            {/* Toggle link */}
+            <div className="text-center py-2">
               {newAccount ? (
                 <p>
                   Already have an account?{" "}
@@ -210,18 +188,6 @@ const Login = () => {
                 </p>
               )}
             </div>
-            <div className="divider">OR</div>
-            {/* Google signin */}
-            <button
-              onClick={handleGoogleSignIn}
-              className="w-full flex items-center justify-center gap-2 rounded p-2 
-             border border-zinc-200 bg-zinc-200 text-black shadow-lg cursor-pointer 
-             hover:bg-zinc-100 hover:border-zinc-100 hover:text-zinc-500 hover:shadow
-             transition duration-300 ease-in-out"
-            >
-              <FcGoogle className="text-xl" />
-              Google SignIn
-            </button>
           </div>
         </div>
       </div>
