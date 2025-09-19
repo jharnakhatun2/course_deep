aad8cb9621eb865891fdab3b3f1a8675566745d2
jharnakhatun2 <jharnakhatun203@gmail.com>
2025-09-19 19:34:45 +0600

    Added blogApi and blogSlice

--- src/features/blog/blogApi.ts
+++ src/features/blog/blogApi.ts
@@ -1,5 +1,49 @@
 import { apiSlice } from "../api/apiSlice";
 
 export const blogApi = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({})
-})
+  endpoints: (builder) => ({
+    getBlogs: builder.query({
+      query: () => "/blogs",
+      providesTags: ["Blogs"],
+    }),
+
+    getBlogById: builder.query({
+      query: (id) => `/blogs/${id}`,
+      providesTags: (id) => [{ type: "Blogs", id }],
+    }),
+
+    addBlog: builder.mutation({
+      query: (newBlog) => ({
+        url: "/blogs",
+        method: "POST",
+        body: newBlog,
+      }),
+      invalidatesTags: ["Blogs"],
+    }),
+
+    updateBlog: builder.mutation({
+      query: ({ id, ...patch }) => ({
+        url: `/blogs/${id}`,
+        method: "PATCH",
+        body: patch,
+      }),
+      invalidatesTags: (id) => [{ type: "Blogs", id }],
+    }),
+
+    deleteBlog: builder.mutation({
+      query: (id) => ({
+        url: `/blogs/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Blogs"],
+    }),
+  }),
+});
+
+export const {
+  useGetBlogsQuery,
+  useGetBlogByIdQuery,
+  useAddBlogMutation,
+  useUpdateBlogMutation,
+  useDeleteBlogMutation,
+} = blogApi;
--- src/features/blog/blogSlice.ts
+++ src/features/blog/blogSlice.ts
@@ -1,11 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {};
-export const blogSlice = createSlice({
-    name : "blog",
-    initialState,
-    reducers: {}
-})
+const initialState = {
+  selectedBlog: null, // Example: keep track of selected blog
+};
 
-export const {} = blogSlice.actions;
+export const blogSlice = createSlice({
+  name: "blog",
+  initialState,
+  reducers: {
+    setSelectedBlog: (state, action) => {
+      state.selectedBlog = action.payload;
+    },
+    clearSelectedBlog: (state) => {
+      state.selectedBlog = null;
+    },
+  },
+});
+
+export const { setSelectedBlog, clearSelectedBlog } = blogSlice.actions;
 export default blogSlice.reducer;
