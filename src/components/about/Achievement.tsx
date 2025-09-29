3b9f6d1b5d320b2dfad097acb358fd8d0fef4105
jharnakhatun2 <jharnakhatun203@gmail.com>
2025-09-29 21:00:43 +0600

    Added responsive achievement section

--- src/components/about/Achievement.tsx
+++ src/components/about/Achievement.tsx
@@ -1,17 +1,17 @@
 const achievements = [
-    { value: "37+", label: "Peer-Reviewed Papers" },
-    { value: "84+", label: "Patents Granted" },
-    { value: "22M+", label: "Daily Predictions" },
-    { value: "98%", label: "Client Retention" },
+    { value: "150+", label: "Courses" },
+    { value: "30K+", label: "Students" },
+    { value: "180+", label: "Instructors" },
+    { value: "10K+", label: "Certificates" },
   ];
 
 const Achievement = () => {
   return (
-    <section className="bg-gray-500 text-white p-12 mb-20">
+    <section className="bg-gray-500 text-white p-5 lg:p-12 mb-20">
           <div className="grid grid-cols-2 md:grid-cols-4 gap-8 text-center">
             {achievements.map((item, idx) => (
               <div key={idx}>
-                <div className="text-5xl font-bold text-yellow-400 mb-2">
+                <div className="text-3xl lg:text-5xl font-bold text-yellow-400 mb-2">
                   {item.value}
                 </div>
                 <div className="text-gray-300">{item.label}</div>
