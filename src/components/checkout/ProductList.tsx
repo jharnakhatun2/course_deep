79590fbef1c7923e9c0e01a145fc12a37fde7250
jharnakhatun2 <jharnakhatun203@gmail.com>
2025-10-09 13:17:19 +0600

    Modify Checkout all components

--- src/components/checkout/ProductList.tsx
+++ src/components/checkout/ProductList.tsx
@@ -3,14 +3,14 @@ import image from "../../assets/img/hero/hero-3.webp";
 const ProductList = () => {
   return (
     <div className="overflow-x-auto">
-      <div className="flex justify-between items-center border-b border-gray-300 ">
+      <div className="flex justify-between items-center border-b border-gray-300 space-y-5 ">
         <div className="font-medium relative">
           <img
             src={image}
             alt="course"
             className="w-20 object-cover mx-auto my-2"
           />
-          <span className="absolute top-0 -right-6 border w-5 h-5 rounded-full flex items-center justify-center text-black">
+          <span className="absolute top-0 -right-6 border w-4 h-4 sm:w-5 sm:h-5  rounded-full flex items-center justify-center text-black">
             3
           </span>
         </div>
--- src/pages/CheckOut.tsx
+++ src/pages/CheckOut.tsx
@@ -25,7 +25,7 @@ const CheckOut = () => {
   const labelStyle = "flex items-center gap-2";
   return (
     <section className="py-10 bg-gray-100">
-      <div className="lg:max-w-7xl mx-auto px-4 grid grid-cols-1 lg:grid-cols-5 gap-20">
+      <div className="lg:max-w-7xl mx-auto px-4 grid grid-cols-1 lg:grid-cols-5 gap-10">
         {/* Billing Details */}
         <div className="lg:col-span-3">
           {/* Form */}
