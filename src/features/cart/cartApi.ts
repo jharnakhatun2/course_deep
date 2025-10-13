548c913484d8f19c79aed899887f7adb12cdf415
jharnakhatun2 <jharnakhatun203@gmail.com>
2025-10-14 04:17:21 +0600

    Modify Cart Api for email query

--- src/features/cart/cartApi.ts
+++ src/features/cart/cartApi.ts
@@ -4,12 +4,15 @@ import { apiSlice } from "../api/apiSlice";
 
 export const cartApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    getCart: builder.query<CartItem[], void>({
-      query: () => "/cart",
+    getCart: builder.query<CartItem[], string | void>({
+      query: (userEmail) => ({
+        url: userEmail ? `/cart?userEmail=${userEmail}` : '/cart',
+        method: "GET",
+      }),
       providesTags: ["Cart"],
     }),
 
-    addToCart: builder.mutation<CartItem, { productId: string; quantity: number; type: 'course' | 'event'; }>({
+    addToCart: builder.mutation<CartItem, { productId: string; quantity: number; type: 'course' | 'event'; userEmail: string;}>({
       query: (cartItem) => ({
         url: "/cart",
         method: "POST",
@@ -18,26 +21,26 @@ export const cartApi = apiSlice.injectEndpoints({
       invalidatesTags: ["Cart"],
     }),
 
-    removeFromCart: builder.mutation<{ success: boolean }, { productId: string; type: string }>({
-      query: ({ productId, type }) => ({
-        url: `/cart/${productId}?type=${type}`,
+    removeFromCart: builder.mutation<{ success: boolean }, { productId: string; type: string; userEmail?: string; }>({
+      query: ({ productId, type, userEmail }) => ({
+        url: `/cart/${productId}?type=${type}${userEmail ? `&userEmail=${userEmail}` : ''}`,
         method: "DELETE",
       }),
       invalidatesTags: ["Cart"],
     }),
 
-    updateCartQuantity: builder.mutation<CartItem, { productId: string; type: string; quantity: number }>({
-      query: ({ productId, type, quantity }) => ({
+    updateCartQuantity: builder.mutation<CartItem, { productId: string; type: string; quantity: number; userEmail?: string; }>({
+      query: ({ productId, type, quantity, userEmail }) => ({
         url: `/cart/${productId}`,
         method: "PATCH",
-        body: { type, quantity },
+        body: { type, quantity, userEmail },
       }),
       invalidatesTags: ["Cart"],
     }),
 
-    clearCart: builder.mutation<{ success: boolean }, void>({
-      query: () => ({
-        url: "/cart/clear",
+    clearCart: builder.mutation<{ success: boolean }, {userEmail?: string}>({
+      query: ({userEmail}={}) => ({
+        url: userEmail ?  `/cart/clear?userEmail=${userEmail}` : "/cart/clear",
         method: "DELETE",
       }),
       invalidatesTags: ["Cart"],
